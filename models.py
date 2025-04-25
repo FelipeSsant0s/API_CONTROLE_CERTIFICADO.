ff5662454b66dc9f1c09db797091b10254f61a31
Felipe silva santos <113404338+FelipeSsant0s@users.noreply.github.com>
2025-04-25 13:24:56 -0300

    Update0.3

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     certificados = db.relationship('Certificado', backref='user', lazy=True)
 
     def set_password(self, password):
